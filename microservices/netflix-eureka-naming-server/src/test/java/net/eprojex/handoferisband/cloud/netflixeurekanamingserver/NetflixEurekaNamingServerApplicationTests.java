4214ee6f07d7d2deacb48dfec58671941cbebc62
Your Name <you@example.com>
2020-07-17 09:14:29 -0400

    Adding API Gateway, API Proxy, load balancer, service discovery, currency exchange service, and setup H2 embedded DB

---
+++ microservices/netflix-eureka-naming-server/src/test/java/net/eprojex/handoferisband/cloud/netflixeurekanamingserver/NetflixEurekaNamingServerApplicationTests.java
@@ -0,0 +1,13 @@
+package net.eprojex.handoferisband.cloud.netflixeurekanamingserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NetflixEurekaNamingServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
---
+++ microservices/netflix-zuul-api-gateway-server/src/test/java/net/eprojex/handoferisband/cloud/netflixzuulapigatewayserver/NetflixZuulApiGatewayServerApplicationTests.java
@@ -0,0 +1,13 @@
+package net.eprojex.handoferisband.cloud.netflixzuulapigatewayserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NetflixZuulApiGatewayServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
